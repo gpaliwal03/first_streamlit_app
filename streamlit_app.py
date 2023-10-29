457c20dacd3c395fda145e56719699f2e0d9e6b2
gpaliwal03 <149031863+gpaliwal03@users.noreply.github.com>
2023-10-29 08:15:15 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,13 +36,14 @@ streamlit.dataframe(my_fruit_list)
 
 #pre-populate the list
 
-fruits_selected = streamlit.multiselect("Pick some fruits :",list(my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.multiselect("Pick some fruits :",list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
 
 
 #Filter the Table Data
+fruits_selected = streamlit.multiselect("Pick some fruits :",list(my_fruit_list.index),['Avocado','Strawberries'])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
