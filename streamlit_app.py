dcf2f90daa84325c1996a66b18414c6c1b4744f6
gpaliwal03 <149031863+gpaliwal03@users.noreply.github.com>
2023-11-04 08:21:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,6 +88,15 @@ except URLError as e:
     streamlit.error()
 
 streamlit.header("Function calling!")
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
+
+
     
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
