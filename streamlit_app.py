f223cc8f15ba0277a698e762d4e81be783009072
gpaliwal03 <149031863+gpaliwal03@users.noreply.github.com>
2023-11-04 08:34:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Function calling!")
 
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
